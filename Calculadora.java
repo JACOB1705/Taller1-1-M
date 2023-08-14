9f79486b9405ad70fb5e658ae376ea6b30d8f2cc
JACOB1705 <141367280+JACOB1705@users.noreply.github.com>
2023-08-14 18:11:55 -0500

    Primer Commit change

--- Calculadora.java
+++ Calculadora.java
@@ -1,16 +1,24 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.calculadora;
+import java.util.Scanner;
 
-/**
- *
- * @author Jacobo
- */
-public class Calculadora {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+public class calculadora {
+    public static void main (String[] args){
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Calculadora - Operaciones disponibles:");
+        System.out.println("1) Suma");
+        System.out.println("2) Resta");
+        System.out.println("3) Multiplicacion");
+        System.out.println("4) Division");
+        System.out.println("5) Seno");
+        System.out.println("6) Coseno");
+        System.out.println("7) Tangente");
+        System.out.println("8) Raiz enesima");
+        System.out.println("9) Potencia enesima");
+        System.out.println("10) Calcular IVA");
+        
+        System.out.println("Seleccione ua operacion a realizar (1-10): ");
+        int opcion = input.nextInt();
+        
     }
 }
