3d9aae914ce2ce9c99034034983be3b15e0a9f93
JACOB1705 <141367280+JACOB1705@users.noreply.github.com>
2023-08-14 18:41:15 -0500

    Segundo Commit Change

--- Calculadora.java
+++ Calculadora.java
@@ -1,7 +1,7 @@
 package com.mycompany.calculadora;
 import java.util.Scanner;
 
-public class calculadora {
+public class Calculadora {
     public static void main (String[] args){
         Scanner input = new Scanner(System.in);
         
@@ -20,5 +20,41 @@ public class calculadora {
         System.out.println("Seleccione ua operacion a realizar (1-10): ");
         int opcion = input.nextInt();
         
+        switch (opcion){
+            case 1: 
+                System.out.println("Ingrese el primer numero: ");
+                double num1 = input.nextDouble();
+                System.out.println("Ingrese el primer numero: ");
+                double num2 = input.nextDouble();
+                System.out.println("Resultado: "+(num1+num2));
+                break;
+            case 2:
+                System.out.println("Ingrese el primer numero: ");
+                num1 = input.nextDouble();
+                System.out.println("Ingrese el primer numero: ");
+                num2 = input.nextDouble();
+                System.out.println("Resultado: "+(num1-num2));
+                break;
+            case 3:
+                System.out.println("Ingrese el primer numero: ");
+                num1 = input.nextDouble();
+                System.out.println("Ingrese el primer numero: ");
+                num2 = input.nextDouble();
+                System.out.println("Resultado: "+(num1*num2));
+                break;
+            case 4:
+                System.out.println("Ingrese el primer numero: ");
+                num1 = input.nextDouble();
+                System.out.println("Ingrese el primer numero: ");
+                num2 = input.nextDouble();
+                if(num2!=0){
+                    System.out.println("Resultado: "+(num1/num2));
+                }else{
+                    System.out.println("Resultado: invalido, no es divisible en 0 ");
+                }
+                
+                
+                break;            
+        }
     }
 }
